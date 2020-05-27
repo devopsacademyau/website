47f3f53f19f06b711e55fda51d5e21ee9428f55a
Caio Trevisan <me@caiotrevisan.com>
2020-05-27 19:10:09 +1000

    update registration closed to website; add leadership code

---
+++ leaderboard/precache-manifest.bad070a33785c0771c001415a4c5dbb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bfc2b3490e7c27b0b6e49ad25b7455",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d94a0be3c4a74d4b07e",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4d94a0be3c4a74d4b07e",
+    "url": "/static/js/2.a6347eb3.chunk.js"
+  },
+  {
+    "revision": "e5616bdd0c0f91fc9569",
+    "url": "/static/js/main.0f35ad82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
